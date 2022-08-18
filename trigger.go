a861b3808ad6343d994b7d2594e602ef68a23a21
Emilis <emilis.mitrikas@gmail.com>
2022-08-18 20:58:04 +0100

    Added IDer interface (only to be used with Event struct) Added another private field in the Trigger struct called "eventHistory". It will store all the IDs of the Events that it's gone through. Added comments to all the methods. Added new method "SetValue", it sets a new value for the trigger. Added new method "AddEventId", this will add the event to the eventHistory.

--- trigger.go
+++ trigger.go
@@ -4,22 +4,57 @@ import (
 	"sync"
 )
 
-type Trigger[TVal any] struct {
-	val TVal
-	mx  sync.RWMutex
+//===========[INTERFACES]====================================================================================================
+
+type IDer interface {
+	Id() string
 }
 
+//===========[STRUCTS]====================================================================================================
+
+//Trigger stores the value to be sent to subscribers
+type Trigger[TVal any] struct {
+	val          TVal
+	eventHistory map[string]struct{}
+	mx           sync.RWMutex
+}
+
+//Value returns the value passed along with the trigger
 func (t *Trigger[TVal]) Value() TVal {
 	t.mx.RLock()
 	defer t.mx.RUnlock()
 	return t.val
 }
 
+//SetValue replaces the value in the trigger
+func (t *Trigger[TVal]) SetValue(v TVal) {
+	t.mx.Lock()
+	defer t.mx.Unlock()
+	t.val = v
+}
+
+//AddEventId adds a new event ID to internal cache. This is used later to prevent infinite loops from forming when
+//subscribers call other events that call the initial event and in turn starting the infinite loop
+func (t *Trigger[TVal]) AddEventId(id string) {
+	t.mx.Lock()
+	defer t.mx.Unlock()
+	t.eventHistory[id] = struct{}{}
+}
+
+//CreateEventException allows you to pass this trigger back to the event for the second time
+//func (t *Trigger[TVal]) CreateEventException(id string) {
+//	t.mx.Lock()
+//	defer t.mx.Unlock()
+//	t.eventHistory[id] = struct{}{}
+//}
+
 //===========[FUNCTIONALITY]====================================================================================================
 
+//NewTrigger initiates and returns a new Trigger
 func NewTrigger[TVal any](v TVal) Trigger[TVal] {
 	return Trigger[TVal]{
-		val: v,
-		mx:  sync.RWMutex{},
+		val:          v,
+		eventHistory: make(map[string]struct{}),
+		mx:           sync.RWMutex{},
 	}
 }
