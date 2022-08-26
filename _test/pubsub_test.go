e7837979e88657a7ade07446ac95f487b8eb914e
Emilis <emilis.mitrikas@gmail.com>
2022-08-26 18:50:36 +0100

    Added test "Event_Publish"

--- _test/pubsub_test.go
+++ _test/pubsub_test.go
@@ -16,3 +16,23 @@ func Test_New(t *testing.T) {
 		t.Errorf("Got wrong type. Expected %s, got %s", expected, got)
 	}
 }
+
+func TestEvent_Publish(t *testing.T) {
+	e := pubsub.New[string]()
+	expecting := "hello_world"
+	got := ""
+
+	callback := func(s string) {
+		got = expecting
+	}
+
+	e.Subscribe(callback)
+
+	wg := e.Publish(expecting)
+
+	wg.Wait()
+
+	if got != expecting {
+		t.Errorf("In TestEvent_Publish expeted to have %s, got %s", expecting, got)
+	}
+}
