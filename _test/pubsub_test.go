44755f1d0a22b86a752b839f6a080c099d4d0d9c
Emilis <emilis.mitrikas@gmail.com>
2022-08-29 19:45:55 +0100

    Added test for "Event_Unsubscribe"

--- _test/pubsub_test.go
+++ _test/pubsub_test.go
@@ -86,3 +86,33 @@ func TestSubscription_Cancel(t *testing.T) {
 		t.Errorf("After subscription cancellation, expected to have same values in variables `got` and `expected`, got %s and %s", expecting, got)
 	}
 }
+
+func TestEvent_Unsubscribe(t *testing.T) {
+	e := pubsub.New[string]()
+	expecting := "hello_world"
+	got := ""
+
+	callback := func(s string) {
+		got = expecting
+	}
+
+	subscription := e.Subscribe(callback)
+
+	wg := e.Publish(expecting)
+
+	wg.Wait()
+
+	if got != expecting {
+		t.Errorf("Expected to have `got` and `expecting` variables to be the same, got %s and %s", expecting, got)
+	}
+
+	e.Unsubscribe(subscription.Id())
+
+	wg = e.Publish("new_value")
+
+	wg.Wait()
+
+	if got != expecting {
+		t.Errorf("After unsubscription, expected to have same values in variables `got` and `expected`, got %s and %s", expecting, got)
+	}
+}
